52552168d2636eceecee82e52a5c3199558af25e
geha <ospac111@gmail.com>
2025-09-25 17:14:01 +0900

    chore: eslint, prettier 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,61 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { sheriff } from "eslint-config-sheriff";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const sheriffOptions = {
+  react: true,
+  lodash: false,
+  remeda: false,
+  next: true,
+  astro: false,
+  playwright: false,
+  storybook: true,
+  jest: false,
+  vitest: false,
+};
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...sheriff(sheriffOptions),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: false,
+        },
+      ],
+      "no-console": "off",
+      // 절대경로 통일을 위해 ".*" 패턴 제한
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: [".*"],
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-floating-promises": "off",
+      "react-refresh/only-export-components": "off",
+      "react/jsx-no-useless-fragment": "off",
+      "@typescript-eslint/naming-convention": "off",
+      "jsdoc/require-description-complete-sentence": "off",
+      "func-style": "off",
+      "no-negated-condition": "off",
+      "unicorn/consistent-function-scoping": "off",
+      "react/no-multi-comp": "off",
+      "@typescript-eslint/no-use-before-define": "off",
+      "@typescript-eslint/require-await": "off",
+      "tsdoc/syntax": "off",
+      "fsecond/valid-event-listener": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "fsecond/prefer-destructured-optionals": "off",
+      "@typescript-eslint/no-misused-spread": "off",
+      "@typescript-eslint/prefer-nullish-coalescing": "off",
+    },
   },
 ];
-
 export default eslintConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
 import "@/app/globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Live Trip",
