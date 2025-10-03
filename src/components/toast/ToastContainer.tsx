0b149dc9484805db9bb9db78ce8218d52dc0dea3
geha <ospac111@gmail.com>
2025-10-04 02:30:39 +0900

    feat: modal과 toast를 동시에 띄울시 toast가 먼저 뜨도록 popover 구현으로 변경

--- src/components/toast/ToastContainer.tsx
+++ src/components/toast/ToastContainer.tsx
@@ -1,9 +1,9 @@
 'use client';
 import Image from 'next/image';
-import { type ReactNode, useEffect, useState } from 'react';
+import { type ReactNode, useEffect, useRef, useState } from 'react';
 import errorImage from '@/components/toast/assets/error.svg';
 import successImage from '@/components/toast/assets/success.svg';
-import type { ToastType } from '@/components/toast/type';
+import type { ToastComponentProps, ToastType } from '@/components/toast/type';
 
 export const toast = ({ message, eventType }: ToastType) => {
   const event = new CustomEvent('showToast', {
@@ -12,59 +12,106 @@ export const toast = ({ message, eventType }: ToastType) => {
 
   window.dispatchEvent(event);
 };
-
 const TOAST_DURATION = 3000;
 
 export default function ToastContainer() {
   const [toasts, setToasts] = useState<ToastType[]>([]);
 
+  const deleteToastByIndex = (deletedIndex: number) => {
+    setToasts((prev) => prev.filter((_, i) => i !== deletedIndex));
+  };
+
   useEffect(() => {
-    const toastHandler = (e: CustomEventInit) => {
+    const handleShowToast = (e: CustomEventInit) => {
       const { message, eventType } = e.detail;
 
       setToasts((prev) => [...prev, { message, eventType }]);
-      console.log(toasts);
-      //NOTE: TOAST_DURATION이후 toasts 제거
-      // setTimeout(() => {
-      //   setToasts((prev) => prev.slice(1));
-      // }, TOAST_DURATION);
+      //NOTE: TOAST_DURATION이 흐르면 해당 toast 제거
+      setTimeout(() => {
+        setToasts((prev) => prev.slice(1));
+      }, TOAST_DURATION);
     };
 
-    window.addEventListener('showToast', toastHandler);
+    window.addEventListener('showToast', handleShowToast);
 
     return () => {
-      window.removeEventListener('showToast', toastHandler);
+      window.removeEventListener('showToast', handleShowToast);
     };
   }, [toasts]);
 
   return (
     <>
-      <div className='absolute top-5 right-5 flex flex-col gap-2'>
-        {toasts.map((_toast) => {
+      <div
+        className='absolute top-5 right-5 flex flex-col gap-2'
+        id='toast-container'
+      >
+        {toasts.map((_toast, index) => {
           return (
-            <div
+            <ToastComponent
               key={`toast-${crypto.randomUUID()}`}
-              className='flex-center text-18 gap-5 rounded-[1.875rem] bg-white px-8 py-4 font-medium shadow-sm'
-            >
-              <span>{getEventTypeImage(_toast.eventType)}</span>
-              <span>{_toast.message}</span>
-            </div>
+              eventType={_toast.eventType}
+              message={_toast.message}
+              index={index}
+              deleteToastByIndex={deleteToastByIndex}
+            />
           );
         })}
       </div>
     </>
   );
 }
-const getEventTypeImage = (eventType: string): ReactNode => {
-  switch (eventType) {
-    case 'success': {
-      return <Image src={successImage} alt='체크 이미지' />;
+
+function ToastComponent({
+  eventType,
+  message,
+  index,
+  deleteToastByIndex,
+}: ToastComponentProps) {
+  const toastRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const currentToast = toastRef.current;
+
+    if (!currentToast) {
+      return;
     }
-    case 'error': {
-      return <Image src={errorImage} alt='X 이미지' />;
+    currentToast.showPopover();
+  }, []);
+
+  const getEventTypeImage = (): ReactNode => {
+    switch (eventType) {
+      case 'success': {
+        return <Image src={successImage} alt='체크 이미지' />;
+      }
+      case 'error': {
+        return <Image src={errorImage} alt='X 이미지' />;
+      }
+      default: {
+        return null;
+      }
     }
-    default: {
-      return null;
-    }
-  }
-};
+  };
+
+  return (
+    <div
+      ref={toastRef}
+      popover='manual'
+      className='flex-center text-18 inset-auto right-5 left-auto gap-6 bg-white px-8 py-4 font-medium shadow-sm'
+      style={{
+        top: `${20 + index * 70}px`,
+      }}
+    >
+      <div className='flex gap-4'>
+        <span>{getEventTypeImage()}</span>
+        <span>{message}</span>
+      </div>
+      <button
+        onClick={() => {
+          deleteToastByIndex(index);
+        }}
+      >
+        X
+      </button>
+    </div>
+  );
+}
--- src/components/toast/type.ts
+++ src/components/toast/type.ts
@@ -2,3 +2,7 @@ export interface ToastType {
   message: string;
   eventType: 'success' | 'error';
 }
+export interface ToastComponentProps extends ToastType {
+  index: number;
+  deleteToastByIndex: (index: number) => void;
+}
