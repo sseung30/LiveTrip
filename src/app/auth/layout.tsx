dd9c01a02fa1160d697c8463cab6b334bd195c30
geha <ospac111@gmail.com>
2025-10-05 15:39:22 +0900

    feat: auth의 layout 추가

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,9 @@
+import type { ReactNode } from 'react';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <div className='px-6 py-14 md:px-12 md:py-36'>{children}</div>
+    </>
+  );
+}
