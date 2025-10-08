070fabc312cc25b922ea0fc0583a576c660896f7
aNN-algorithm <kyukyu0314@naver.com>
2025-10-09 02:10:04 +0900

    docs: useInfiniteScroll 훅 로직 설명 추가

--- src/hooks/useInfiniteScroll.tsx
+++ src/hooks/useInfiniteScroll.tsx
@@ -6,22 +6,32 @@ interface useInfiniteScrollProps {
   setPage: (callback: (prevPage: number) => number) => void;
 }
 
+/**
+ * loading: 현재 추가 데이터를 불러오는 중인지 여부
+ * hasMore: 더 불러올 데이터가 남았는지 여부
+ * setPage: 페이지 번호를 증가시키는 상태 업데이트 함수
+ */
 export default function useInfiniteScroll({
   loading,
   hasMore,
   setPage,
 }: useInfiniteScrollProps) {
+  // 실제로 화면 스크롤 상태를 관찰하는 IntersectionObserver 인스턴스
   const observer = useRef<IntersectionObserver | null>(null);
+  // 리스트 하단의 로딩 트리거 <div>에 연결
   const loader = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
+    // 이미 요청을 보내는 중이면(로딩 중이면), 관찰자를 만들지 않음
     if (loading) {
       return;
     }
+    // 이미 관찰 중이면, 관찰 연결 중단
     if (observer.current) {
-      observer.current.disconnect(); // 중복 확인 제거
+      observer.current.disconnect();
     }
 
+    // 새로운 IntersectionObserver
     observer.current = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
@@ -36,6 +46,7 @@ export default function useInfiniteScroll({
       }
     );
 
+    // loader div를 관찰 대상으로 등록
     if (loader.current) {
       observer.current.observe(loader.current);
     }
@@ -47,5 +58,6 @@ export default function useInfiniteScroll({
     };
   }, [loading, hasMore, setPage]);
 
+  // loader ref를 받아 <div ref={loader} />등에 연결할 수 있도록 반환
   return { loader };
 }
