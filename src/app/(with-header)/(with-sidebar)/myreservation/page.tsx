aefdcf0e8f8a97a77f15c695db345205474b7d98
aNN-algorithm <kyukyu0314@naver.com>
2025-10-14 20:26:21 +0900

    feat: 예약 내역 UI 내 예약 취소 모달 컴포넌트 연결 - #41

--- src/app/(with-header)/(with-sidebar)/myreservation/page.tsx
+++ src/app/(with-header)/(with-sidebar)/myreservation/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
+import { useActionState, useState } from 'react';
 import Button from '@/components/button/Button';
 import CardList from '@/components/cardList/CardList';
+import { AlertModalContents } from '@/components/dialog';
+import { ModalContainer } from '@/components/dialog/Modal/ModalContainer';
+import { useDialog } from '@/components/dialog/useDialog';
 import type { StateType } from '@/components/stateBadge/type';
-import mockData from '@/mocks/mockReservations2.json';
+import mockData from '@/mocks/mockReservations.json';
 
 const STATUSES = [
   '예약 승인',
@@ -15,6 +18,15 @@ const STATUSES = [
   '체험 완료',
 ];
 
+const deleteAction = async () => {
+  console.log('삭제 실행');
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1500);
+  });
+
+  return { state: 'success' };
+};
+
 export default function Page() {
   const [selectedStatus, setSelectedStatus] = useState<string | null>(null);
 
@@ -22,74 +34,113 @@ export default function Page() {
 
   const hasReservations = Boolean(totalCount);
 
-  return (
-    <main className='w-full'>
-      <section className='pb-30'>
-        <div className='mb-3 md:mb-7.5'>
-          <h2 className='text-18 mb-2.5 font-bold text-gray-950'>예약내역</h2>
-          <p className='text-gray-500'>예약내역 변경 및 취소할 수 있습니다.</p>
-        </div>
-        {hasReservations && (
-          <div role='group' className='mb-3 flex gap-2 md:mb-7.5'>
-            {STATUSES.map((status) => {
-              const isSelected = selectedStatus === status;
+  const [state, formAction, isPending] = useActionState(deleteAction, {
+    state: ' ',
+  });
+  const cancelReservationDialog = useDialog();
+  const writeReviewDialog = useDialog();
 
-              return (
-                <Button
-                  key={status}
-                  variant='filter'
-                  style={isSelected ? 'accent' : 'default'}
-                  classNames='w-[90px]'
-                  onClick={() => {
-                    setSelectedStatus(status);
-                  }}
-                >
-                  {status}
-                </Button>
-              );
-            })}
+  const onChangeReservation = () => {
+    console.log('예약 변경');
+  };
+
+  const onCancelReservation = () => {
+    cancelReservationDialog.openDialog();
+  };
+
+  return (
+    <>
+      <ModalContainer dialogRef={cancelReservationDialog.dialogRef}>
+        <AlertModalContents
+          message='예약을 취소하시겠어요?'
+          confirmButtonText='취소하기'
+          rejectButtonText='아니오'
+          hideModal={cancelReservationDialog.hideDialog}
+          confirmAction={formAction}
+          isPending={isPending}
+        />
+      </ModalContainer>
+      <ModalContainer dialogRef={writeReviewDialog.dialogRef}></ModalContainer>
+
+      <main className='w-full'>
+        <section className='pb-30'>
+          <div className='mb-3 md:mb-7.5'>
+            <h2 className='text-18 mb-2.5 font-bold text-gray-950'>예약내역</h2>
+            <p className='text-gray-500'>
+              예약내역 변경 및 취소할 수 있습니다.
+            </p>
           </div>
-        )}
-        <div className='flex h-full flex-col gap-6 overflow-y-auto'>
-          {hasReservations &&
-            reservations.map((r) => {
-              return (
-                <div key={r.id}>
-                  <CardList
-                    state={r.status as StateType}
-                    title={r.activity.title}
-                    date={r.date}
-                    startTime={r.startTime}
-                    endTime={r.endTime}
-                    price={r.totalPrice}
-                    capacity={10}
-                  />
-                </div>
-              );
-            })}
-          {!hasReservations && (
-            <div className='mt-2.5 flex flex-col'>
-              <div className='flex items-center justify-center'>
-                <Image
-                  src='/images/reservation_empty.png'
-                  alt='empty'
-                  width={122}
-                  height={122}
-                  className='mb-7.5'
-                />
-              </div>
-              <p className='text-18 mb-7.5 flex justify-center font-medium text-gray-600'>
-                아직 예약한 체험이 없어요
-              </p>
-              <div className='flex justify-center'>
-                <Button variant='primary' classNames='w-[182px]'>
-                  둘러보기
-                </Button>
-              </div>
+          {hasReservations && (
+            <div role='group' className='mb-3 flex gap-2 md:mb-7.5'>
+              {STATUSES.map((status) => {
+                const isSelected = selectedStatus === status;
+
+                return (
+                  <Button
+                    key={status}
+                    variant='filter'
+                    style={isSelected ? 'accent' : 'default'}
+                    classNames='w-[90px]'
+                    onClick={() => {
+                      setSelectedStatus(status);
+                    }}
+                  >
+                    {status}
+                  </Button>
+                );
+              })}
             </div>
           )}
-        </div>
-      </section>
-    </main>
+          <div className='flex h-full flex-col gap-6 overflow-y-auto'>
+            {hasReservations &&
+              reservations.map((r) => {
+                return (
+                  <div key={r.id}>
+                    <CardList
+                      state={r.status as StateType}
+                      title={r.activity.title}
+                      date={r.date}
+                      startTime={r.startTime}
+                      endTime={r.endTime}
+                      price={r.totalPrice}
+                      capacity={10}
+                      onChangeReservation={() => {
+                        onChangeReservation();
+                      }}
+                      onCancelReservation={() => {
+                        onCancelReservation();
+                      }}
+                      onWriteReview={() => {
+                        console.log('후기 작성');
+                      }}
+                    />
+                  </div>
+                );
+              })}
+            {!hasReservations && (
+              <div className='mt-2.5 flex flex-col'>
+                <div className='flex items-center justify-center'>
+                  <Image
+                    src='/images/reservation_empty.png'
+                    alt='empty'
+                    width={122}
+                    height={122}
+                    className='mb-7.5'
+                  />
+                </div>
+                <p className='text-18 mb-7.5 flex justify-center font-medium text-gray-600'>
+                  아직 예약한 체험이 없어요
+                </p>
+                <div className='flex justify-center'>
+                  <Button variant='primary' classNames='w-[182px]'>
+                    둘러보기
+                  </Button>
+                </div>
+              </div>
+            )}
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
