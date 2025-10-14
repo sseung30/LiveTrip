fc91b8544cbb090c7f4357ae2747593973383092
aNN-algorithm <kyukyu0314@naver.com>
2025-10-14 16:33:41 +0900

    design: 불필요한 div 태그 제거 및 레이아웃 조정 - #42

--- src/app/(with-header)/(with-sidebar)/myactivities/page.tsx
+++ src/app/(with-header)/(with-sidebar)/myactivities/page.tsx
@@ -16,67 +16,65 @@ export default function Page() {
   };
 
   return (
-    <div>
-      <main className='w-full'>
-        <section className='pb-30'>
-          <div className='flex items-center justify-between'>
-            <div>
-              <h2 className='text-18 mb-2.5 font-bold text-gray-950'>
-                내 체험 관리
-              </h2>
-              <p className='text-gray-500'>
-                체험을 등록하거나 수정 및 삭제가 가능합니다.
-              </p>
-            </div>
-            <div className='hidden sm:block'>
-              <Button
-                variant='primary'
-                classNames='w-[138px]'
-                onClick={handleClick}
-              >
-                체험 등록하기
-              </Button>
-            </div>
+    <main className='w-full'>
+      <section className='pb-30'>
+        <div className='flex items-center justify-between'>
+          <div>
+            <h2 className='text-18 mb-2.5 font-bold text-gray-950'>
+              내 체험 관리
+            </h2>
+            <p className='text-gray-500'>
+              체험을 등록하거나 수정 및 삭제가 가능합니다.
+            </p>
           </div>
-          <div className='scrollbar-hide flex h-full flex-col gap-6 overflow-y-auto'>
-            {hasActivities &&
-              activities.map((a) => {
-                return (
-                  <div key={a.id}>
-                    <ActivitiyCard
-                      title={a.title}
-                      rating={a.rating}
-                      reviewCount={a.reviewCount}
-                      price={a.price}
-                      bannerImageUrl={a.bannerImageUrl}
-                    />
-                  </div>
-                );
-              })}
-            {!hasActivities && (
-              <div className='mt-2.5 flex w-119 flex-col'>
-                <div className='flex justify-center'>
-                  <Image
-                    src='/images/reservation_empty.png'
-                    alt='empty'
-                    width={122}
-                    height={122}
-                    className='mb-7.5'
+          <div className='hidden sm:block'>
+            <Button
+              variant='primary'
+              classNames='w-[138px]'
+              onClick={handleClick}
+            >
+              체험 등록하기
+            </Button>
+          </div>
+        </div>
+        <div className='scrollbar-hide flex h-full flex-col gap-6 overflow-y-auto'>
+          {hasActivities &&
+            activities.map((a) => {
+              return (
+                <div key={a.id}>
+                  <ActivitiyCard
+                    title={a.title}
+                    rating={a.rating}
+                    reviewCount={a.reviewCount}
+                    price={a.price}
+                    bannerImageUrl={a.bannerImageUrl}
                   />
                 </div>
-                <p className='text-18 mb-7.5 flex justify-center font-medium text-gray-600'>
-                  아직 예약한 체험이 없어요
-                </p>
-                <div className='flex justify-center'>
-                  <Button variant='primary' classNames='w-[182px]'>
-                    둘러보기
-                  </Button>
-                </div>
+              );
+            })}
+          {!hasActivities && (
+            <div className='mt-7.5 flex flex-col'>
+              <div className='flex items-center justify-center'>
+                <Image
+                  src='/images/reservation_empty.png'
+                  alt='empty'
+                  width={122}
+                  height={122}
+                  className='mb-7.5'
+                />
               </div>
-            )}
-          </div>
-        </section>
-      </main>
-    </div>
+              <p className='text-18 mb-7.5 flex justify-center font-medium text-gray-600'>
+                아직 예약한 체험이 없어요
+              </p>
+              <div className='flex justify-center'>
+                <Button variant='primary' classNames='w-[182px]'>
+                  둘러보기
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+      </section>
+    </main>
   );
 }
