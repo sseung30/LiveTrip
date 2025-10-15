c324645290f71d9b1c96b1c27882313dba2585a9
geha <ospac111@gmail.com>
2025-10-15 22:34:48 +0900

    refactor: next-auth.d.ts 주석 제거

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,10 +1,6 @@
 import type { User } from 'next-auth';
 
 declare module 'next-auth' {
-  /**
-   * The user information we expect to be able to extract
-   * from our decoded backend tokens
-   */
   export interface UserObject {
     id: number;
     nickname: string;
@@ -12,53 +8,25 @@ declare module 'next-auth' {
     profileImageUrl: string | null;
   }
 
-  /**
-   * The contents of our refresh call to the backend is a new access
-   * token
-   */
   export interface BackendAccessJWT {
     access: string;
   }
 
-  /**
-   * The initial backend authentication response contains both an `access`
-   * token and a `refresh` token. The refresh token is a long-lived token
-   * that is used to obtain a new access token when the current access token
-   * expires
-   */
   export interface BackendJWT extends BackendAccessJWT {
     refresh: string;
   }
 
-  /**
-   * The decoded contents of a JWT token returned from the backend
-   * (both access and refresh tokens). It contains both the user
-   * information and other token metadata.
-   * `iat` is the time the token was issued,
-   * `exp` is the time the token expires
-   * `jti` is the token id.
-   */
   export interface DecodedJWT extends UserObject {
     exp: number;
     iat: number;
     id: number;
   }
 
-  /**
-   * Information extracted from our decoded backend tokens so that we
-   * don't need to decode them again.
-   * `validUntil` is the time the access token becomes invalid
-   * `refreshUntil` is the time the refresh token becomes invalid
-   */
   export interface AuthValidity {
     validUntil: number;
     refreshUntil: number;
   }
 
-  /**
-   * The returned data from the authorize method
-   * This is data we extract from our own backend JWT tokens.
-   */
   export interface User {
     refreshId: number;
     tokens: BackendJWT;
@@ -66,11 +34,7 @@ declare module 'next-auth' {
     validity: AuthValidity;
     error?: string;
   }
-  /**
-   * Returned by `useSession`, `getSession`, returned by the `session`
-   * callback and also the shape received as a prop on the SessionProvider
-   * React Context
-   */
+
   export interface Session {
     user: UserObject;
     validity: AuthValidity;
@@ -80,9 +44,6 @@ declare module 'next-auth' {
 }
 
 declare module 'next-auth/jwt' {
-  /**
-   * Returned by the `jwt` callback and `getToken`, when using JWT sessions
-   */
   export interface JWT {
     data: User;
     error: 'RefreshTokenExpired' | 'RefreshAccessTokenError';
