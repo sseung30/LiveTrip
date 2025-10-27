e8bd8d5c5d91fadb2f2c25a7a5b915ac985704cf
aNN-algorithm <kyukyu0314@naver.com>
2025-10-28 03:50:23 +0900

    feat: 무한 스크롤 렌더링

--- src/app/(with-header)/(with-sidebar)/myreservation/page.tsx
+++ src/app/(with-header)/(with-sidebar)/myreservation/page.tsx
@@ -1,16 +1,6 @@
-import { apiFetch } from '@/api/api';
 import MyReservationsSection from '@/domain/myreservations/MyReservationsSection';
-import type { MyReservations } from '@/domain/myreservations/type';
-// import mockData from '@/mocks/mockReservations.json';
 
 export default async function Page() {
-  // const { reservations, totalCount } = mockData;
-
-  const { reservations, totalCount } =
-    await apiFetch<MyReservations>('/my-reservations');
-
-  const hasReservations = Boolean(totalCount);
-
   return (
     <>
       <main className='w-full'>
@@ -18,10 +8,7 @@ export default async function Page() {
           <h2 className='text-18 mb-2.5 font-bold text-gray-950'>예약내역</h2>
           <p className='text-gray-500'>예약내역 변경 및 취소할 수 있습니다.</p>
         </div>
-        <MyReservationsSection
-          hasReservations={hasReservations}
-          reservations={reservations}
-        />
+        <MyReservationsSection />
       </main>
     </>
   );
